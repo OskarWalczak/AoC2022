0b75aee90666d65cda8d3fc4330c840aa89c959c
OskarWalczak <owalczak@bee2code.com>
2022-12-02 10:29:54 +0100

    day01 moved to new repo

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
+
+    fun generateListOfSums(lines: List<String>): List<Int> {
+        var currentSum: Int = 0
+
+        val sums : MutableList<Int> = ArrayList<Int>()
+
+        lines.forEach{line ->
+            if(line.isEmpty()){
+                sums.add(currentSum)
+                currentSum = 0
+            }
+            else{
+                val cals: Int = Integer.parseInt(line)
+                currentSum += cals
+            }
+        }
+
+        sums.sortDescending()
+        return sums
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return generateListOfSums(input)[0]
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sums = generateListOfSums(input)
+        return sums[0] + sums[1] + sums[2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
